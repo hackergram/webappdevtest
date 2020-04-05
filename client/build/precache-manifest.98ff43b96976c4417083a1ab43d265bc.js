5e432af22b6edacdc35779fafa355566e19ff4bc
Shah Quadri <30766213+shah78677@users.noreply.github.com>
2020-04-06 00:41:59 +0530

    updated map style

---
+++ client/build/precache-manifest.98ff43b96976c4417083a1ab43d265bc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf697827efcf5f2cf7b0f7eab8e78789",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02297a4932e082056e44",
+    "url": "/static/css/main.d1939d0d.chunk.css"
+  },
+  {
+    "revision": "7154856885ba324ba888",
+    "url": "/static/js/2.d6d45376.chunk.js"
+  },
+  {
+    "revision": "67ec752e578893db050e41ae792e22e0",
+    "url": "/static/js/2.d6d45376.chunk.js.LICENSE"
+  },
+  {
+    "revision": "02297a4932e082056e44",
+    "url": "/static/js/main.8b946c79.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  },
+  {
+    "revision": "5ebb24ee1112dd9562629375c387879a",
+    "url": "/static/media/Roboto-Black.5ebb24ee.ttf"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "6bc3acea783745482f8f9ec47d484e95",
+    "url": "/static/media/Signika-Bold.6bc3acea.ttf"
+  },
+  {
+    "revision": "a50ed734b74d1f77538e1559192d0421",
+    "url": "/static/media/close.a50ed734.svg"
+  },
+  {
+    "revision": "6992069bbf19f28c476eb6b9f821a6f1",
+    "url": "/static/media/info.6992069b.svg"
+  },
+  {
+    "revision": "7a17a52de705cddc0900c72c2f8a624f",
+    "url": "/static/media/twitter.7a17a52d.svg"
+  }
+]);
